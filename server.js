6eaa85942245f800797241d6c157b36ad17b7cc2
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2024-08-12 21:33:40 +0530

    update codes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
-const app = express()
+const app = express();
+const db = require('./db');
+require('dotenv').config();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); //req.body
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log("listening on 3000")
-}) 
+// Import the router files
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
+
+// Use the routers
+app.use('/user', userRoutes);
+app.use('/candidate', candidateRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
